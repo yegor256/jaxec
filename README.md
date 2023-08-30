619f168dd214194ca833be317214860258a5c2c2
Yegor Bugayenko <yegor256@gmail.com>
2023-08-30 17:11:12 +0300

    #1 start

---
+++ README.md
@@ -0,0 +1,44 @@
+[![EO principles respected here](https://www.elegantobjects.org/badge.svg)](https://www.elegantobjects.org)
+[![DevOps By Rultor.com](http://www.rultor.com/b/yegor256/jaxec)](http://www.rultor.com/p/yegor256/jaxec)
+[![We recommend IntelliJ IDEA](https://www.elegantobjects.org/intellij-idea.svg)](https://www.jetbrains.com/idea/)
+
+[![mvn](https://github.com/yegor256/jaxec/actions/workflows/mvn.yml/badge.svg)](https://github.com/yegor256/jaxec/actions/workflows/mvn.yml)
+[![PDD status](http://www.0pdd.com/svg?name=yegor256/jaxec)](http://www.0pdd.com/p?name=yegor256/jaxec)
+[![Maven Central](https://img.shields.io/maven-central/v/com.yegor256/jaxec.svg)](https://maven-badges.herokuapp.com/maven-central/com.yegor256/jaxec)
+[![Javadoc](http://www.javadoc.io/badge/com.yegor256/jaxec.svg)](http://www.javadoc.io/doc/com.yegor256/jaxec)
+[![codecov](https://codecov.io/gh/yegor256/jaxec/branch/master/graph/badge.svg)](https://codecov.io/gh/yegor256/jaxec)
+[![Hits-of-Code](https://hitsofcode.com/github/yegor256/jaxec)](https://hitsofcode.com/view/github/yegor256/jaxec)
+![Lines of code](https://img.shields.io/tokei/lines/github/yegor256/jaxec)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/yegor256/jaxec/blob/master/LICENSE.txt)
+
+It's a simple executor of a command line command from Java.
+
+You add this to your `pom.xml`:
+
+```xml
+<dependency>
+  <groupId>com.yegor256</groupId>
+  <artifactId>jaxec</artifactId>
+</dependency>
+```
+
+Then, just do this:
+
+```java
+String stdout = new Jaxec("ls", "-al", "/tmp").exec();
+```
+
+Should work.
+
+## How to Contribute
+
+Fork repository, make changes, send us a [pull request](https://www.yegor256.com/2014/04/15/github-guidelines.html).
+We will review your changes and apply them to the `master` branch shortly,
+provided they don't violate our quality standards. To avoid frustration,
+before sending us your pull request please run full Maven build:
+
+```bash
+$ mvn clean install -Pqulice
+```
+
+You will need Maven 3.3+ and Java 8+.
---
+++ src/main/java/com/yegor256/Jaxec.java
@@ -0,0 +1,97 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.yegor256;
+
+import com.jcabi.log.Logger;
+import com.jcabi.log.VerboseProcess;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.cactoos.io.InputOf;
+import org.cactoos.io.OutputTo;
+import org.cactoos.io.TeeInput;
+import org.cactoos.scalar.IoChecked;
+import org.cactoos.scalar.LengthOf;
+
+/**
+ * Java command line executor.
+ *
+ * @since 0.1.0
+ */
+public final class Jaxec {
+
+    /**
+     * Arguments.
+     */
+    private final List<String> arguments;
+
+    /**
+     * Ctor.
+     * @param args The command line arguments
+     */
+    public Jaxec(final String... args) {
+        this.arguments = Arrays.asList(args);
+    }
+
+    /**
+     * Execute it and return the output.
+     * @return Stdout and stderr together
+     */
+    public String exec() {
+        try {
+            return this.execUnsafe();
+        } catch (final IOException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    /**
+     * Execute it and return the output.
+     * @return Stdout and stderr together
+     * @throws IOException If fails
+     */
+    public String execUnsafe() throws IOException {
+        Logger.debug(this, "+%s", this.arguments);
+        final Process proc = new ProcessBuilder()
+            .command(this.arguments)
+            .directory(new File(System.getProperty("user.dir")))
+            .redirectErrorStream(true)
+            .start();
+        final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        try (VerboseProcess vproc = new VerboseProcess(proc)) {
+            new IoChecked<>(
+                new LengthOf(
+                    new TeeInput(
+                        new InputOf(vproc.stdoutQuietly()),
+                        new OutputTo(stdout)
+                    )
+                )
+            ).value();
+        }
+        return stdout.toString();
+    }
+
+}
